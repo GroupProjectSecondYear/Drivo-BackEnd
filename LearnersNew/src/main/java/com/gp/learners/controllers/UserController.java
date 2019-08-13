4f3d63b998d506a440b26ec63543ef52130af531
Nuwan <nuwanmadhusankarulz@gmail.com>
2019-08-13 20:33:38 +0530

    Initial change

--- LearnersNew/src/main/java/com/gp/learners/controllers/UserController.java
+++ LearnersNew/src/main/java/com/gp/learners/controllers/UserController.java
@@ -1,6 +1,6 @@
 package com.gp.learners.controllers;
 
-
+//Hello
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
--- LearnersNew/src/main/java/com/gp/learners/service/StudentService.java
+++ LearnersNew/src/main/java/com/gp/learners/service/StudentService.java
@@ -2,14 +2,36 @@ package com.gp.learners.service;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gp.learners.repositories.StudentPackageRepository;
+import com.gp.learners.repositories.StudentRepository;
 
 
 
 @Service
 public class StudentService {
 	
+	@Autowired
+	StudentPackageRepository studentPackageRepository;
+	
+	@Autowired 
+	StudentRepository studentRepository;
+	
 	public Boolean register(Map<String, String> data) {
 		return false;
+	}
+	
+	
+	//give student followed packages
+	public Object packageList(Integer id){
+		
+		if(studentRepository.existsById(id)) {
+			//System.out.println(studentPackageRepository.getStudentDetailsId(id));
+		}
+		
+		
+		return null;
 	}
 }
